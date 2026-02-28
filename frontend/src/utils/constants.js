671ae6d2c2cd6be11933af6d337d2a85357524d2
Vishal Singh <TechnicalVishalJi@users.noreply.github.com>
2026-02-28 11:38:42 +0530

    fixed direct url not found issue

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api/v1";
+export const API_BASE_URL = "https://cryptomarket-fk5h.onrender.com/api/v1";
 
 export const NFT_CONTRACT_ADDRESS =
   "0xa47fE06d737b64626deA8cd0a638E64ce5a4daFa";
